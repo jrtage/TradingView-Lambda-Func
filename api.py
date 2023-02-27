fec98adaf29cabf077d2992332d6364dfd5b32e3
jrtage <jrtage@gmail.com>
2023-02-27 11:26:26 -0600

    tidied up

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 keys = {
-    'api': 'API Key',
-    'secret': b'Secret Key',
+    'api': 'account-xVcci5i7ZOEjZQ05oGmY',
+    'secret': b'44YWohaW9DhQM3gVEA6cndFQ9DhB',
     'balUrl': 'https://api.sandbox.gemini.com/v1/balances',
     'tradeUrl': 'https://api.sandbox.gemini.com/v1/order/new',
     'priceUrl': 'https://api.gemini.com/v1/pubticker/'
